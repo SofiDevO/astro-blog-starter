8c647a206b25300d7b76921d91e2643147181d13
SofiDev <angiemeow89@gmail.com>
2025-04-03 22:24:52 -0600

    ✨ Template completada

--- src/data/menuElements.js
+++ src/data/menuElements.js
@@ -1,24 +1,41 @@
 export const MenuItems = [
-    { link: "#", text: "Inicio" },
-    {
-      link: "#",
-      text: "Noticias",
-      submenu: [
-        { link: "#", text: "Mas Leídas" },
-        { link: "#", text: "Tendencias" },
-        { link: "#", text: "Nacionales" },
-      ],
-    },
-    { link: "#", text: "Negocios" },
-    { link: "#", text: "Patata" },
-    { link: "#", text: "Tecnología" },
-    { link: "#", text: "Moda" },
-    {
-      link: "#",
-      text: "Salud",
-      submenu: [
-          { link: "#", text: "Fitness", submenu: [{ link: "#", text: "Eventos" }, { link: "#", text: "Dopados" }, { link: "#", text: "12" }]  },
-          { link: "#", text: "Salud", submenu: [{ link: "#", text: "Eventos" }, { link: "#", text: "Dopados" }, { link: "#", text: "12" }]  }, { link: "#", text: "12" },
-      ],
-    },
-  ];
+  { link: "/", text: "Inicio" ,  submenu: null},
+  {
+    link: "#",
+    text: "Noticias",
+    submenu: [
+      { link: "#", text: "Mas Leídas" || null },
+      { link: "#", text: "Tendencias"  || null},
+      { link: "#", text: "Nacionales"  || null},
+    ],
+  },
+  { link: "#", text: "Negocios",  submenu: null },
+  { link: "#", text: "hamburguesas",  submenu: null },
+  { link: "#", text: "Tecnología",  submenu: null },
+  { link: "#", text: "Moda",  submenu: null},
+  {
+    link: "#" || null,
+    text: "Salud" || null,
+    submenu: [
+      {
+        link: "#",
+        text: "Fitness",
+        submenu: [
+          { link: "#", text: "Eventos"  || null},
+          { link: "#", text: "Dopados" || null },
+          { link: "#", text: "12" || null },
+        ],
+      },
+      {
+        link: "#" || null,
+        text: "Salud" || null,
+        submenu: [
+          { link: "#", text: "Eventos"  || null},
+          { link: "#", text: "Dopados"  || null},
+          { link: "#", text: "12"  || null},
+        ],
+      },
+      { link: "#", text: "12" ,  submenu: [] },
+    ],
+  },
+];
